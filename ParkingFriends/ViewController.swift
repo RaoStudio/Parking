a6f55513ce8c3f511408fa50b750ab0937e99297
MyoungHyoun Cho <>
2018-03-05 14:06:03 +0900

    Fixes

--- ParkingFriends/ViewController.swift
+++ ParkingFriends/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        // Commit Test ~ 
+        // Commit Test ~ @@
     }
 
     override func didReceiveMemoryWarning() {
