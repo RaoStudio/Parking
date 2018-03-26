623cfeb322ef7c91582c75371fec9b2f16375219
Rao <munk1004@hanmail.net>
2018-03-26 11:13:56 +0900

    'SideMenu' Test ~~

---
+++ ParkingFriends/MainViewController.swift
@@ -0,0 +1,45 @@
+//
+//  MainViewController.swift
+//  ParkingFriends
+//
+//  Created by MyoungHyoun Cho on 2018. 3. 26..
+//  Copyright © 2018년 rao. All rights reserved.
+//
+
+import UIKit
+import SideMenu
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    // MARK: - Side
+    fileprivate func setupSideMenu() {
+        SideMenuManager.default.menuLeftNavigationController = storyboard!.instantiateViewController(withIdentifier: "LeftMenuNavigationController") as? UISideMenuNavigationController;
+        
+        SideMenuManager.default.menuAddPanGestureToPresent(toView: self.navigationController!.navigationBar)
+        SideMenuManager.default.menuAddScreenEdgePanGesturesToPresent(toView: self.navigationController!.view);
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
