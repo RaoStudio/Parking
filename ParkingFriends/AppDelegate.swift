3b7b7cdf992add63cf4d264d8dd1c382d8466b9f
Rao <munk1004@hanmail.net>
2018-04-02 14:44:27 +0900

    Test Code for Google Map ( CLLocationManagerDelegate ) ~

--- ParkingFriends/AppDelegate.swift
+++ ParkingFriends/AppDelegate.swift
@@ -10,6 +10,8 @@ import UIKit
 import CoreData
 import GoogleMaps
 
+let googleApiKey = "AIzaSyBwqFR308aG7E02HTXsopaJmGTUbmPFMI8"
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
@@ -19,7 +21,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GMSServices.provideAPIKey("AIzaSyBwqFR308aG7E02HTXsopaJmGTUbmPFMI8")
+        GMSServices.provideAPIKey("")
+        GMSServices.provideAPIKey(googleApiKey)
         
         return true
     }
--- ParkingFriends/MainViewController.swift
+++ ParkingFriends/MainViewController.swift
@@ -12,12 +12,20 @@ import SideMenu
 import GoogleMaps
 
 class MainViewController: UIViewController {
-
+    
+    @IBOutlet var mapView: GMSMapView!
+    
+    private let locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         setupSideMenu()
+//        setupMap()
+        
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,13 +33,14 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    override func loadView() {
+//    override func loadView() {
+    func setupMap() {
         // Create a GMSCameraPosition that tells the map to display the
         // coordinate -33.86,151.20 at zoom level 6.
         let camera = GMSCameraPosition.camera(withLatitude: -33.86, longitude: 151.20, zoom: 6.0)
-        let mapView = GMSMapView.map(withFrame: CGRect.zero, camera: camera)
+        let mapView = GMSMapView.map(withFrame: self.view.frame, camera: camera)
         mapView.isMyLocationEnabled = true
-        view = mapView
+        self.view.addSubview(mapView)
         
         // Creates a marker in the center of the map.
         let marker = GMSMarker()
@@ -65,4 +74,46 @@ class MainViewController: UIViewController {
     }
     */
 
+}
+
+
+extension MainViewController: CLLocationManagerDelegate {
+    
+    /*
+     *  locationManager:didChangeAuthorizationStatus:
+     *
+     *  Discussion:
+     *    Invoked when the authorization status changes for this application.
+     */
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        guard status == CLAuthorizationStatus.authorizedWhenInUse else {
+            return
+        }
+        
+        locationManager.startUpdatingLocation()
+        
+        mapView.isMyLocationEnabled = true
+        mapView.settings.myLocationButton = true
+    }
+    
+    /*
+     *  locationManager:didUpdateLocations:
+     *
+     *  Discussion:
+     *    Invoked when new locations are available.  Required for delivery of
+     *    deferred locations.  If implemented, updates will
+     *    not be delivered to locationManager:didUpdateToLocation:fromLocation:
+     *
+     *    locations is an array of CLLocation objects in chronological order.
+     */
+    @available(iOS 6.0, *)
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            return
+        }
+        
+        mapView.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+        locationManager.stopUpdatingLocation()
+    }
+    
 }
