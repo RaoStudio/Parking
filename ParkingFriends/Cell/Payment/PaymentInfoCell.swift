0bbffdf55a371c6814a232e3eabb153aeb062ab5
Rao_iMac <munk1004@hanmail.net>
2018-07-24 16:14:06 +0900

    PaymentVC : Adding Cells ~ Ing

---
+++ ParkingFriends/Cell/Payment/PaymentInfoCell.swift
@@ -0,0 +1,27 @@
+//
+//  PaymentInfoCell.swift
+//  ParkingFriends
+//
+//  Created by Misco on 2018. 7. 24..
+//  Copyright © 2018년 rao. All rights reserved.
+//
+
+import UIKit
+
+class PaymentInfoCell: UITableViewCell {
+
+    @IBOutlet weak var lbl_Title: UILabel!
+    @IBOutlet weak var lbl_Contents: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ParkingFriends/Cell/Payment/PaymentSelectCell.swift
@@ -0,0 +1,24 @@
+//
+//  PaymentSelectCell.swift
+//  ParkingFriends
+//
+//  Created by Misco on 2018. 7. 24..
+//  Copyright © 2018년 rao. All rights reserved.
+//
+
+import UIKit
+
+class PaymentSelectCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
