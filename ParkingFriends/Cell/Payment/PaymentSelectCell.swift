8c5ed452b99b5ce7f6caac6503b544b1ce5f1615
Rao_iMac <munk1004@hanmail.net>
2018-07-24 17:46:35 +0900

    PaymentVC : Ing ~ ( Cell ~ )

--- ParkingFriends/Cell/Payment/PaymentSelectCell.swift
+++ ParkingFriends/Cell/Payment/PaymentSelectCell.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
+protocol PaymentSelectProtocol {
+ 
+    func onPaymentSelct(nTag: Int) 
+}
+
 class PaymentSelectCell: UITableViewCell {
 
+    var nTag: Int = 0
+    var delegate: PaymentSelectProtocol?
+    
+    @IBOutlet weak var btnCreditCard: UIButton!
+    @IBOutlet weak var btnCellular: UIButton!
+    @IBOutlet weak var btnAccount: UIButton!
+    @IBOutlet weak var btnKakao: UIButton!
+    
+    
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
@@ -20,5 +35,21 @@ class PaymentSelectCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    
+    
+    @IBAction func onBtnSelect(_ sender: UIButton) {
+        
+        btnCreditCard.isSelected = false
+        btnCellular.isSelected = false
+        btnAccount.isSelected = false
+        btnKakao.isSelected = false
+        
+        
+        sender.isSelected = true
+        
+        self.nTag = sender.tag
+        
+        self.delegate?.onPaymentSelct(nTag: self.nTag)
+    }
+    
 }
