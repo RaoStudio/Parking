b04789f9649bfb42bfe5d305f700b276b579a682
Rao_iMac <munk1004@hanmail.net>
2018-07-25 09:57:34 +0900

    Create PaymentCoupSelectCell ~

---
+++ ParkingFriends/Cell/Payment/PaymentCoupSelectCell.swift
@@ -0,0 +1,27 @@
+//
+//  PaymentCoupSelectCell.swift
+//  ParkingFriends
+//
+//  Created by Misco on 2018. 7. 25..
+//  Copyright © 2018년 rao. All rights reserved.
+//
+
+import UIKit
+
+class PaymentCoupSelectCell: UITableViewCell {
+
+    @IBOutlet weak var lbl_Title: UILabel!
+    @IBOutlet weak var lbl_Count: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
