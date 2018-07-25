256e9848fb3bd0e8a92e810043aa1a5d67fa4084
Rao_iMac <munk1004@hanmail.net>
2018-07-25 10:15:03 +0900

    Create PaymentTotalCountCell ~

---
+++ ParkingFriends/Cell/Payment/PaymentTotalCountCell.swift
@@ -0,0 +1,26 @@
+//
+//  PaymentTotalCountCell.swift
+//  ParkingFriends
+//
+//  Created by Misco on 2018. 7. 25..
+//  Copyright © 2018년 rao. All rights reserved.
+//
+
+import UIKit
+
+class PaymentTotalCountCell: UITableViewCell {
+
+    @IBOutlet weak var lbl_TotalCount: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
