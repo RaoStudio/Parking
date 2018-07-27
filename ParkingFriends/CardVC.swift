ad750e30b38872de4cc9e27e732aef840e8db6d4
Rao_iMac <munk1004@hanmail.net>
2018-07-27 16:07:34 +0900

    CardVC :  Storyboard Ing ~

--- ParkingFriends/CardVC.swift
+++ ParkingFriends/CardVC.swift
@@ -8,14 +8,29 @@
 
 import UIKit
 
-class CardVC: UIViewController {
+class CardVC: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var contentsView: UIView!
+    @IBOutlet weak var txtCard_One: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
         self.navigationItem.title = "카드 결제"
+        
+        txtCard_One.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillChange(notification:)),
+                                               name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide),
+                                               name: NSNotification.Name.UIKeyboardWillHide, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,7 +38,45 @@ class CardVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
+    // MARK: NotificationCenter
+    @objc func keyboardWillHide() {
+        self.contentsView.frame.origin.y = 0
+    }
+    
+    @objc func keyboardWillChange(notification: NSNotification) {
+        
+        if let keyboardSize = (notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            
+            self.contentsView.frame.origin.y = -(keyboardSize.height/2)
+            
+            /*
+             if (txtCarName.isFirstResponder || txtCarNum.isFirstResponder ){
+             self.view.frame.origin.y = -(keyboardSize.height/2)
+             }
+             */
+        }
+    }
 
+    
+    // MARK: - UITextField Delegate
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        /*
+        if textField.isEqual(self.txtCarName) {
+            self.txtCarNum.becomeFirstResponder()
+        } else {
+            tapMyView(self.view)
+        }
+ */
+        
+        txtCard_One.resignFirstResponder()
+        
+        return true
+    }
+    
+    
     /*
     // MARK: - Navigation
 
