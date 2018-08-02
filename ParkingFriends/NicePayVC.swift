0350b1ce2b7f0dcfed46fbd007ce5003245e5e5e
Rao_iMac <munk1004@hanmail.net>
2018-08-02 17:27:55 +0900

    Create NicePayVC ~

---
+++ ParkingFriends/NicePayVC.swift
@@ -0,0 +1,37 @@
+//
+//  NicePayVC.swift
+//  ParkingFriends
+//
+//  Created by Misco on 2018. 8. 2..
+//  Copyright © 2018년 rao. All rights reserved.
+//
+
+import UIKit
+
+class NicePayVC: UIViewController {
+
+    @IBOutlet weak var contentsView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
