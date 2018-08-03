9badc06dd85c7570b1e5a4f6b7facd8567a06dad
Rao_iMac <munk1004@hanmail.net>
2018-08-03 10:27:24 +0900

    NicePayVC : WKWebView Test ~

--- ParkingFriends/NicePayVC.swift
+++ ParkingFriends/NicePayVC.swift
@@ -13,6 +13,7 @@ class NicePayVC: UIViewController {
 
     @IBOutlet weak var contentsView: UIView!
     var webView: WKWebView?
+    
     var host = "http://m.daum.net"
     
     override func viewDidLoad() {
@@ -20,7 +21,7 @@ class NicePayVC: UIViewController {
 
         // Do any additional setup after loading the view.
         
-        self.webView = initWebView()
+        webView = initWebView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,20 +29,46 @@ class NicePayVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    
 
+    
     func initWebView() -> WKWebView {
         
         let config = WKWebViewConfiguration()
-        
         let webView:WKWebView = WKWebView(frame: self.contentsView.frame, configuration: config)
+        
+        
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        
+        self.contentsView.addSubview(webView)
+        
+        
+        /*
+        if #available(iOS 11.0, *) {
+            let safeArea = self.view.safeAreaLayoutGuide
+            webView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor).isActive = true
+            webView.topAnchor.constraint(equalTo: safeArea.topAnchor).isActive = true
+            webView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor).isActive = true
+            webView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor).isActive = true
+            
+        } else {
+            
+            
+        }
+ */
+        
+        webView.leadingAnchor.constraint(equalTo: contentsView.leadingAnchor).isActive = true
+        webView.topAnchor.constraint(equalTo: contentsView.topAnchor).isActive = true
+        webView.trailingAnchor.constraint(equalTo: contentsView.trailingAnchor).isActive = true
+        webView.bottomAnchor.constraint(equalTo: contentsView.bottomAnchor).isActive = true
+        
         let url = NSURL(string: host)
         
         let request = NSURLRequest(url: url! as URL)
         webView.load(request as URLRequest)
-        self.contentsView.addSubview(webView)
+        
         
         return webView
-        
     }
     /*
     // MARK: - Navigation
