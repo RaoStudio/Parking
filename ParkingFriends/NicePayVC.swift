6d8d3663ef819f635b055198222e10bf2391a169
Rao_iMac <munk1004@hanmail.net>
2018-08-03 11:42:12 +0900

    NicePayVC : WKWebView Test ~

--- ParkingFriends/NicePayVC.swift
+++ ParkingFriends/NicePayVC.swift
@@ -14,7 +14,12 @@ class NicePayVC: UIViewController {
     @IBOutlet weak var contentsView: UIView!
     var webView: WKWebView?
     
-    var host = "http://m.daum.net"
+//    var host = "http://m.daum.net"
+    var host = UrlStrings.URL_API_NICEPAY_REQUEST
+   
+   
+    var param = "PayMethod=CELLPHONE&BuyerName=%EB%AF%B8%EB%9E%98%EC%97%94%EC%94%A8%ED%8B%B0&BuyerTel=01036638266&BuyerEmail=misconct6161%40gmail.com&member_sid=8&parkinglot_sid=7874&reserve_type=R&begin=2018-08-03+11%3A20&end=2018-08-03+13%3A20&price_ori=5000&point=0&type=nice_etc&code=3RYF9V32S7UULZTNMV1GITB9"
+ 
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -64,7 +69,15 @@ class NicePayVC: UIViewController {
         
         let url = NSURL(string: host)
         
-        let request = NSURLRequest(url: url! as URL)
+        var request = URLRequest(url: url! as URL)
+        let paramData = param.data(using: .utf8)
+        request.httpMethod = "POST"
+        request.httpBody = paramData
+        
+        request.addValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.setValue(String(paramData!.count), forHTTPHeaderField: "Content-Length")
+        
+        
         webView.load(request as URLRequest)
         
         
