1cdf347338fa24a9b32ff5ff469cea79ff67b230
Rao_iMac <munk1004@hanmail.net>
2018-08-08 10:32:59 +0900

    Create ReservHistoryCell ~

---
+++ ParkingFriends/Cell/Reservation/ReservHistoryCell.swift
@@ -0,0 +1,24 @@
+//
+//  ReservHistoryCell.swift
+//  ParkingFriends
+//
+//  Created by Misco on 2018. 8. 8..
+//  Copyright © 2018년 rao. All rights reserved.
+//
+
+import UIKit
+
+class ReservHistoryCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
