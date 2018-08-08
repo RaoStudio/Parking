7458711b65de68ed6c7f9b54574340686815cfce
Rao_iMac <munk1004@hanmail.net>
2018-08-08 16:12:36 +0900

    ReservHistoryVC : ing ~ ( Using Array Make For TableView ( Data From Api is Not Useuable ~ -_- ;;; ) )

--- ParkingFriends/ReservHistoryVC.swift
+++ ParkingFriends/ReservHistoryVC.swift
@@ -14,6 +14,8 @@ class ReservHistoryVC: UIViewController, UITableViewDataSource, UITableViewDeleg
     @IBOutlet weak var tableView: UITableView!
     
     var arrData = [Dictionary<String, Any>]()
+    var arrMake = [[Dictionary<String, Any>]]()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,12 +56,60 @@ class ReservHistoryVC: UIViewController, UITableViewDataSource, UITableViewDeleg
                 print("requestFetchReservationHistory JSON = \(value)")
                 
                 self.arrData = value as! [Dictionary<String, Any>]
+            
+                self.makeArrayForTableView()
                 
                 self.tableView.reloadData()
             }
         }
     }
     
+    
+    func makeArrayForTableView () {
+        
+        var nIndex: Int = 0
+        var strRegDate: String = ""
+        var setStrDate = Set<String>()
+        
+        for item in arrData {
+            let strTime = item["reg_datetime"] as! String
+            
+            let arrTime = strTime.components(separatedBy: " ")
+            print(arrTime)
+            
+            if let strFirst = arrTime.first {
+                setStrDate.insert(strFirst)
+            }
+        }
+        print("Count of Set is \(setStrDate.count)")
+        
+        
+        for item in setStrDate {
+            var arrInsert = [Dictionary<String, Any>]()
+            arrMake.append(arrInsert)
+        }
+        print(arrMake)
+        
+        for item in arrData {
+            let strTime = item["reg_datetime"] as! String
+            let arrTime = strTime.components(separatedBy: " ")
+            
+            if let strFirst = arrTime.first {
+                if strRegDate.isEmpty {
+                    strRegDate = strFirst
+                }
+                
+                if strRegDate != strFirst {
+                    nIndex = nIndex + 1
+                    strRegDate = strFirst
+                }
+                
+                arrMake[nIndex].append(item)
+            }
+        }
+        
+        print(arrMake)
+    }
     
     // MARK: - TableView
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -75,6 +125,19 @@ class ReservHistoryVC: UIViewController, UITableViewDataSource, UITableViewDeleg
         var cell: UITableViewCell
         
         cell = tableView.dequeueReusableCell(withIdentifier: "ReservHistoryCell")!
+        
+        if let resCell = cell as? ReservHistoryCell {
+            
+            if let dic = arrData[indexPath.row] as? Dictionary<String, Any> {
+                if let img: String = dic["img"] as? String {
+                    let strImg: String = UrlStrings.URL_API_PARKINGLOT_IMG + img
+                    
+                    resCell.ivLot.sd_setImage(with: URL(string: strImg), placeholderImage: UIImage(named: "List_NoImage"))
+                }
+            }
+            
+            
+        }
         
         return cell
     }
