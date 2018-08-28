0f35d702ebac7bd046e8d21fd0d7910b538a18e1
Rao_iMac <munk1004@hanmail.net>
2018-08-28 14:14:07 +0900

    DetailVC : Impossible Time Test ~

--- ParkingFriends/DetailVC.swift
+++ ParkingFriends/DetailVC.swift
@@ -581,7 +581,8 @@ class DetailVC: UIViewController, UIPageViewControllerDataSource {
         
         let uinfo = UserInfoManager()
         
-        for (index, value) in arrImpossible.enumerated() {
+//        for (index, value) in arrImpossible.enumerated() {
+        for (index, value) in arrValidate.enumerated() {
             print("\(index) : \(value)")
             
             let arrStr = value.split(separator: " ").map { String($0) }
@@ -654,6 +655,18 @@ class DetailVC: UIViewController, UIPageViewControllerDataSource {
             
             if let value = response.result.value as? String, false == value.isEmpty {
                 self.arrImpossible = value.components(separatedBy: "/")
+                
+                if self.bUseImpossibleTest {
+//                    self.arrImpossible.append("2018-08-28 00:00:00")
+//                    self.arrImpossible.append("2018-08-28 01:00:00")
+                    self.arrImpossible.append("2018-08-28 12:00:00")
+                    self.arrImpossible.append("2018-08-28 13:00:00")
+                    self.arrImpossible.append("2018-08-28 23:00:00")
+                    self.arrImpossible.append("2018-08-29 01:00:00")
+//                    self.arrImpossible.append("2018-08-29 15:00:00")
+//                    self.arrImpossible.append("2018-08-29 16:00:00")
+                }
+                
                 
                 if self.arrImpossible.count >= 2 {
                     self.calcImpossibleTime(arrTime: self.arrImpossible)
