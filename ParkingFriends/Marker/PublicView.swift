bd5f3b7dcd36c0fb55b673e5f7215fea592abeee
Rao_iMac <munk1004@hanmail.net>
2018-09-03 14:01:04 +0900

    Add NewPublicMarker Image ~

---
+++ ParkingFriends/Marker/PublicView.swift
@@ -0,0 +1,23 @@
+//
+//  PublicView.swift
+//  ParkingFriends
+//
+//  Created by Misco on 2018. 9. 3..
+//  Copyright © 2018년 rao. All rights reserved.
+//
+
+import UIKit
+
+class PublicView: UIView {
+
+    @IBOutlet weak var ivMarker: UIImageView!
+    @IBOutlet weak var lblPrice: UILabel!
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
