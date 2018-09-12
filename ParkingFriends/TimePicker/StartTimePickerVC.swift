9a17a31aa8890ac02d818f499b1762fac6676ca3
Rao_iMac <munk1004@hanmail.net>
2018-09-12 16:54:33 +0900

    StartTimePicker : ~ Ing

--- ParkingFriends/TimePicker/StartTimePickerVC.swift
+++ ParkingFriends/TimePicker/StartTimePickerVC.swift
@@ -24,6 +24,9 @@ class StartTimePickerVC: UIViewController {
     var strOne: String = ""
     var strTwo: String = ""
     
+    
+    var arrDay: [Date] = []
+    
     var arrImpossibleTime: [String]?
     var arrArrangeImpossible: [[String]]?
     
@@ -34,6 +37,27 @@ class StartTimePickerVC: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        let startDate = uinfo.stringToDate(uinfo.startTime!)
+        startPicker.minimumDate = startDate
+        startPicker.date = startDate
+        
+        let firstDate = uinfo.stringToDate(uinfo.firstTime!)
+        let oneDate = uinfo.stringToDate(uinfo.secondTime!)
+        let twoDate = uinfo.stringToDate(uinfo.thirdTime!)
+        
+        arrDay = [firstDate, oneDate, twoDate]
+        
+        strNow = String(format: "%d년%02d월%02d일(%@)", firstDate.year, firstDate.month, firstDate.day, firstDate.weekdayName)
+        strOne = String(format: "%d년%02d월%02d일(%@)", oneDate.year, oneDate.month, oneDate.day, oneDate.weekdayName)
+        strTwo = String(format: "%d년%02d월%02d일(%@)", twoDate.year, twoDate.month, twoDate.day, twoDate.weekdayName)
+        
+        
+        setupStartDropDown()
+        
+        btnStart.setTitle(startDropDown.selectedItem, for: UIControlState.normal)
+        
+        startPicker.addTarget(self, action: #selector(valueChanged(_:)), for: UIControlEvents.valueChanged)
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,12 +65,60 @@ class StartTimePickerVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // MARK: - SetUpDropDown
+    func setupStartDropDown() {
+        
+        startDropDown.textFont = UIFont.systemFont(ofSize: 13)
+        startDropDown.anchorView = self.btnStart
+        startDropDown.bottomOffset = CGPoint(x: 0, y: self.btnStart.bounds.height)
+        
+        startDropDown.dataSource = [
+            strNow,
+            strOne,
+            strTwo
+        ]
+        
+        startDropDown.direction = .bottom
+        startDropDown.selectRow(uinfo.nFirstTime!)
+        
+        startDropDown.selectionAction = { [weak self] (index, item) in
+            
+            if self?.btnStart.title(for: UIControlState.normal) == item {
+                return
+            }
+            
+            //            self?.uinfo.nFirstTime = index
+            
+            self?.btnStart.setTitle(item, for: UIControlState.normal)
+            
+            let nowDate = self?.startPicker.date
+            let selDate = self?.arrDay[index]
+            
+            
+            //            self?.startPicker.date = (self?.arrDay[index])!
+            
+            let nHourGap = (nowDate?.hour)! - (selDate?.hour)!
+            
+            self?.startPicker.date = (self?.arrDay[index])! + nHourGap.hour
+            
+            self?.valueChanged((self?.startPicker)!)
+        }
+    }
     
     
     // MARK: - Btn Action
     @IBAction func onBtnExit(_ sender: Any) {
         self.presentingViewController?.dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func onBtnStart(_ sender: UIButton) {
+        startDropDown.show()
+    }
+    
+    @objc func valueChanged(_ sender: UIDatePicker) {
+        
+    }
+    
     
     
 
