d2f037fc3fbaaaa858a00763d6183b50178203de
Rao_iMac <munk1004@hanmail.net>
2018-09-12 14:14:51 +0900

    Create New TimePickerVC ( StartTimePickerVC )

---
+++ ParkingFriends/TimePicker/StartTimePickerVC.swift
@@ -0,0 +1,35 @@
+//
+//  StartTimePickerVC.swift
+//  ParkingFriends
+//
+//  Created by Misco on 2018. 9. 12..
+//  Copyright © 2018년 rao. All rights reserved.
+//
+
+import UIKit
+
+class StartTimePickerVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
