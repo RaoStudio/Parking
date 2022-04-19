47deb9f52b6c66902f291f4d58cc79af149163be
RaoStudio <munk1004@hanmail.net>
2022-04-19 17:05:39 +0900

    Update ViewController.swift

--- ParkingFriends/ViewController.swift
+++ ParkingFriends/ViewController.swift
@@ -22,6 +22,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    static var memoryUsage: String { var taskInfo = task_vm_info_data_t() var count = mach_msg_type_number_t(MemoryLayout<task_vm_info>.size) / 4 let result: kern_return_t = withUnsafeMutablePointer(to: &taskInfo) { $0.withMemoryRebound(to: integer_t.self, capacity: 1) { task_info(mach_task_self_, task_flavor_t(TASK_VM_INFO), $0, &count) } } var used: UInt64 = 0 if result == KERN_SUCCESS { used = UInt64(taskInfo.phys_footprint) } let total = ProcessInfo.processInfo.physicalMemory let bytesInMegabyte = 1024.0 * 1024.0 let usedMemory: Double = Double(used) / bytesInMegabyte let totalMemory: Double = Double(total) / bytesInMegabyte return String(format: "%.1f MB / %.0f MB", usedMemory, totalMemory) }
 }
 
